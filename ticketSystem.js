3a8d71666a0905a05586956e8a914047a6f31073
Luke3g3 <104209392+Luke3g3@users.noreply.github.com>
2023-01-09 08:48:11 +0100

    change

--- ticketSystem.js
+++ ticketSystem.js
@@ -14,6 +14,12 @@
 
 
     function addNew(){
-        document.getElementById("add_new").innerHTML += '<li class="ticket"> <p class="id">Ticket ID:</p><p class="name">Ticket Name:</p><input type="number" class="number" min="0" id=""> <input type="text" class="centerInput" ><p class="besch">Beschreibung:</p><textarea name="" id="" cols="50" rows="4" class="beschreibung" maxlength="160"></textarea><input type="button" value="Speichern" onclick="alert("Gespeichert")" class="saveBtn"></li><br>';
+        document.getElementById("add_new").innerHTML += '<li class="ticket"> <p class="id">Ticket ID:</p><p class="name" id="name">Ticket Name:</p><input type="number" class="number" min="0" id=""> <input type="text" class="centerInput" ><p class="besch">Beschreibung:</p><textarea name="" id="" cols="50" rows="4" class="beschreibung" maxlength="160"></textarea><input type="button" value="Speichern" onclick="alert("Gespeichert")" class="saveBtn"></li><br>';
     
     };
+
+    function save(){
+        var name = document.getElementById('name').value;
+
+        document.getElementById('ticketName').write(`<option class="optionDown">${name}</option>`)
+    }
